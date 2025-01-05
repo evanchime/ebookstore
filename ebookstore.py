e491122edb7674d1a01a7a69ef32fd6339b949dc
Evan Chime <evanchime@yahoo.co.uk>
2025-01-05 22:51:55 +0000

    Refactor ebookstore.py to wrap the script in a main function, implement the argparse module, functionality to obtain the database details from the command line and logic to establish a connection based on the chosen database type

--- ebookstore.py
+++ ebookstore.py
@@ -17,149 +17,204 @@ Also the program will be using classes and functions in seperate files
 to be imported.
 '''
 
-from sqlite3 import DatabaseError, DataError
-from tabulate import tabulate
+import argparse
 import os
 import sys
-from classes import BookStore
-from functions import get_book, get_book_info, get_book_update_info, \
-    get_book_search_query, return_to_menu, exit_utility
+import csv
+import logging  
+from sqlite3 import DatabaseError as SQLiteDatabaseError
+from mysql.connector.errors import DatabaseError as MySQLDatabaseError
+from classes import BookStoreMySQL, BookStoreSqlite
+from functions import(
+    get_book, get_book_info, get_book_update_info,
+    get_book_search_query, return_to_menu, exit_utility, 
+    get_database_connection_params
+)
+from dotenv import load_dotenv
 
-if len(sys.argv) < 2:
-    print(
-        "An error occurred: Not enough arguments provided. " 
-        "Usage: python3 script_name.py "
-        "database_file [optional_table_records_file]"
+load_dotenv()
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='Connect to a database and perform operations'
     )
-    sys.exit(1)
-    
-database_file = sys.argv[1]
-table_records_file = sys.argv[2] if len(sys.argv) > 2 else None
-
-# Create the directory(s) if it/they doesn't exist
-try:
-    os.makedirs(os.path.dirname(database_file), exist_ok=True)
-except PermissionError:
-    print(
-        "Permission denied.You don't have permission to create directory "
-        f"{os.path.dirname(database_file)}"
+    parser.add_argument(
+        '--connection-url', type=str, help='MySQL connection URL'
     )
-    sys.exit(1)
+    parser.add_argument(
+        '--database-file', type=str, help='Sqlite database file'
+    )
+    parser.add_argument(
+        '--table-records', type=str, help='Predefined table records'
+    )
+    parser.add_argument(
+        '--table-name', type=str, help='Table name. Defaults to book'
+    )
 
-# Create an empty list to store the table records
-table_records = []  
+    args = parser.parse_args()
 
-if table_records_file:  # If predefined table records file is provided
+    table_records = []
 
-    try:
-        with open(table_records_file, 'r') as file:
-            for index, line in enumerate(file):
-                if index == 0:
-                    continue
-                record = line.strip().split('|')
-                table_records.append(
-                    (record[0], record[1], record[2], record[3])
-                )
-    except FileNotFoundError:
-        print(
-            f"File not found: File '{table_records_file}' doesn't exist. "
-            "Check your spelling"
+    if args.table_records:
+        try:
+            with open(args.table_records, 'r') as csvfile:
+                reader = csv.reader(csvfile, delimiter=',', quotechar="'")
+                next(reader)
+                for record in reader:
+                    table_records.append(
+                        (record[0], record[1], record[2], record[3])
+                    )
+        except FileNotFoundError:
+            logging.error( 
+                f"File not found: File {args.table_records} doesn't exist. " 
+                "Check your spelling." )
+            sys.exit(1)
+
+    database_connection_params = None
+    database_file = None
+
+    if os.getenv("MYSQL_CONNECTION_URL"):
+        database_connection_params = get_database_connection_params(
+            os.getenv("MYSQL_CONNECTION_URL")
         )
+    elif args.connection_url:
+        database_connection_params = get_database_connection_params(
+            args.connection_url
+        )
+    elif os.getenv("MYSQL_DATABASE_FILE"):
+        database_file = os.getenv("MYSQL_DATABASE_FILE")
+    elif args.database_file:
+        database_file = args.database_file
+    else:
+        logging.error("No database connection provided. Exiting...")
         sys.exit(1)
 
-try:
-    book_store = BookStore(database_file, table_records)
-except DatabaseError as e:
-    print(e)
-    sys.exit(1)
-except PermissionError as e:
-    print(e)
-    sys.exit(1)
+    if database_connection_params:
+        try:
+            book_store = BookStoreMySQL(
+                database_connection_params, args.table_name, table_records
+            )
+        except MySQLDatabaseError as e:
+            logging.error(e)
+            sys.exit(1)
+    elif database_file:
+        # Create the directory(s) if it/they doesn't exist
+        try:
+            if os.path.dirname(database_file): 
+                os.makedirs(os.path.dirname(database_file), exist_ok=True)
+            book_store = BookStoreSqlite(
+                database_file, args.table_name, table_records
+            )
+        except PermissionError:
+            logging.error(
+                "Permission denied. You don't have permission to create "
+                f"directory {os.path.dirname(database_file)}"
+            )
+            sys.exit(1)
+        except SQLiteDatabaseError as e:
+            logging.error(e)
+            sys.exit(1)
 
-while True:
-    try:
-        menu_1 = input(
-"""\nSelect one of the following options:
-1. Enter book
-2. Update book
-3. Delete book
-4. Search books
-0. Exit
-: """      
-        ).strip()
+    while True:
+        try:
+            menu_1 = input(
+    """\nSelect one of the following options:
+    1. Enter book
+    2. Update book
+    3. Delete book
+    4. Search books
+    0. Exit
+    : """      
+            ).strip()
 
-        if menu_1 == '1':
-            try:
-                # Get the book details the first time from the user
-                book = get_book()
-                
-                # Insert the book details into the database
-                book_store.insert_book(book)
-                
-                # Return to the main menu
-                return_to_menu()
-            except ValueError as e:
-                raise e
-            except DatabaseError as e:
-                raise e
-        elif menu_1 == '2':
-            try:
-                # Get the book details from the user
-                book_info = get_book_info()
-                
-                # Get the book details for update from the user
-                book_update_info = get_book_update_info(book_info)
-                
-                # Update the book details in the database
-                book_store.update_book(book_update_info)
+            if menu_1 == '1':
+                try:
+                    # Get the book details the first time from the user
+                    book = get_book()
+                    
+                    # Insert the book details into the database
+                    book_store.insert_book(book)
+                    
+                    # Return to the main menu
+                    return_to_menu()
+                except ValueError as e:
+                    raise e
+                except SQLiteDatabaseError as e:
+                    raise e
+                except MySQLDatabaseError as e:
+                    raise e
+            elif menu_1 == '2':
+                try:
+                    # Get the book details from the user
+                    book_info = get_book_info()
+                    
+                    # Get the book details for update from the user
+                    book_update_info = get_book_update_info(book_info)
+                    
+                    # Update the book details in the database
+                    book_store.update_book(book_update_info)
 
-                # Return to main menu
-                return_to_menu()
-            except ValueError as e:
-                raise e
-            except DataError as e:
-                raise e
-            except DatabaseError as e:
-                raise e
-        elif menu_1 == '3':
-            try:
-                # Get the book details from the user
-                book_info = get_book_info()    
+                    # Return to main menu
+                    return_to_menu()
+                except ValueError as e:
+                    raise e
+                except SQLiteDatabaseError as e:
+                    raise e
+                except MySQLDatabaseError as e:
+                    raise e
+                except Exception as e:
+                    raise e
+            elif menu_1 == '3':
+                try:
+                    # Get the book details from the user
+                    book_info = get_book_info()    
 
-                # Delete the book details from the database
-                book_store.delete_book(book_info)
-                
-                # Return to main menu
-                return_to_menu()
-            except ValueError as e:
-                raise e
-            except DatabaseError as e:
-                raise e
-        elif menu_1 == '4':
-            try:
-                # Get the book details from the user
-                search_query = get_book_search_query()  
+                    # Delete the book details from the database
+                    book_store.delete_book(book_info)
+                    
+                    # Return to main menu
+                    return_to_menu()
+                except ValueError as e:
+                    raise e
+                except SQLiteDatabaseError as e:
+                    raise e
+                except MySQLDatabaseError as e:
+                    raise e
+            elif menu_1 == '4':
+                try:
+                    # Get the book details from the user
+                    search_query = get_book_search_query()  
 
-                # Search for the book details in the database
-                book_store.search_books(search_query)
-                
-                # Return to main menu
-                return_to_menu()
-            except ValueError as e:
-                raise e
-            except DatabaseError as e:
-                raise e        
-        elif menu_1 == '0':
-            # Exit the application
-            exit_utility(book_store) 
-        else:
-            print("\nInvalid option. Please try again.")
-    except ValueError as e:
-        print('\n', e, sep='')
-        return_to_menu()
-    except DataError as e:
-        print('\n', e, sep='')
-        return_to_menu()
-    except DatabaseError as e:
-        print('\n', e, sep='')
-        return_to_menu()
+                    # Search for the book details in the database
+                    book_store.search_books(search_query)
+                    
+                    # Return to main menu
+                    return_to_menu()
+                except ValueError as e:
+                    raise e
+                except SQLiteDatabaseError as e:
+                    raise e
+                except MySQLDatabaseError as e:
+                    raise e        
+            elif menu_1 == '0':
+                # Exit the application
+                exit_utility(book_store) 
+            else:
+                print("\nInvalid option. Please try again.")
+        except ValueError as e:
+            print('\n', e, sep='')
+            return_to_menu()
+        except SQLiteDatabaseError as e:
+            print('\n', e, sep='')
+            return_to_menu()
+        except MySQLDatabaseError as e:
+            print('\n', e, sep='')
+            return_to_menu()
+        except Exception as e:
+            print('\n', e, sep='')
+            return_to_menu()
+
+
+if __name__ == "__main__":
+    main()
